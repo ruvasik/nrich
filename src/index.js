7674903e9dccb70f53eeee264822b5224ea6c6f6
Vladimir Alekseev <mail@vlal.ru>
2022-07-18 18:09:24 +0300

    routes

--- src/index.js
+++ src/index.js
@@ -3,21 +3,36 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {Login} from "./features/login/Login";
+import {News} from "./features/news/News";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import global from './global.module.css';
+import {Nav} from "./Nav";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+console.log(global);
+
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/news" element={<News />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
