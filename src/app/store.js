eecda716c36c247c34cf2be43996396c9e223461
Vladimir Alekseev <mail@vlal.ru>
2022-07-18 17:50:39 +0300

    history middleware for store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+
 import counterReducer from '../features/counter/counterSlice';
+import loginReducer from '../features/login/loginSlice';
+
+export const history = createBrowserHistory();
 
 export const store = configureStore({
   reducer: {
+    router: connectRouter(history),
     counter: counterReducer,
+    login: loginReducer,
   },
+  middleware: [routerMiddleware(history)],
 });
